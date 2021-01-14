436e056398e6b74b2207db03e1c2668de361c846
Nelirks <raphael.lesbros@laposte.net>
2021-01-14 11:34:33 +0100

    Début implémentation incription.connexion

--- code/login.php
+++ code/login.php
@@ -1,6 +1,12 @@
 <?php require 'utils.inc.php';
 start_page('Login');?>
 
+<?php
+if (isset($_GET['etat']) && $_GET['etat'] == 'CONNEXIONECHOUEE') {
+    echo '<b> Identifiant ou mot de passe invalide, veuillez réessayer. </b> ';
+}
+?>
+
 <!-- Formulaire de connexion-->
 <form action="traitement.php" method="post">
     <fieldset>
@@ -14,10 +20,12 @@ start_page('Login');?>
 <!-- Formulaire d'inscription -->
 <form action="traitement.php" method="post">
     <fieldset>
-        <h3>Toujours pas inscrit ? Inscrivez-vous !!</h3>//faire une redirection vers une page d'inscription ?<br>
-        <input type="text" name="pseudo" placeholder='Pseudo'/><br>
-        <input type="email" name="email" placeholder='Email'><br>
-        <input type="password" name="mdp" placeholder='Mot de passe'/><br>
+        <!-- faire une redirection vers une page d'inscription ? -->
+        <h3>Toujours pas inscrit ? Inscrivez-vous !!</h3><br>
+        <input type="text" name="pseudo" placeholder='Pseudo' required/><br>
+        <input type="email" name="email" placeholder='Email' required><br>
+        <input type="password" name="mdp" placeholder='Mot de passe' pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required  title="Doit contenir au moins un chiffre, une minuscule, une majuscule, et au moins 8 caractères"/><br>
+        <input type="password" name="verificationmdp" placeholder='Confirmation mpd' required><br/>
         <button type="submit" name="action" value="inscription">Inscription</button>
     </fieldset>
 </form>
--- code/traitement.php
+++ code/traitement.php
@@ -1,7 +1,59 @@
 <?php
-$nom = $_POST['pseudo'];
+$pseudo = $_POST['pseudo'];
 $email = $_POST['email'];
 $mdp = $_POST['mdp'];
 $action = $_POST['action'];
+$verificationmdp = $_POST['verificationmdp'];
+
+
+$dbLink = mysqli_connect('localhost', 'root', '')
+or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+mysqli_select_db($dbLink , 'user')
+or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
+
+if ($action == 'connexion') {
+    session_start();
+    $dbResult = mysqli_query($dbLink, 'SELECT password, role FROM user WHERE nom =\'' .$pseudo.'\'');
+    $connected = false;
+    while($dbRow = mysqli_fetch_assoc($dbResult)) {
+        if (md5($mdp) == $dbRow['password']) {
+            $_SESSION['etat'] = 'connecte';
+            $_SESSION['role'] = $dbRow['role'];
+            $connected = true;
+            header('Location: main.php');
+        }
+    }
+    if (!$connected) {
+        header('Location: login.php?etat=CONNEXIONECHOUEE');
+    }
+}
+else if ($action == 'inscription') {
+    $dbLink = mysqli_connect('localhost', 'root', '')
+    or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+    mysqli_select_db($dbLink , 'user')
+    or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
+    if ()
+    $query = 'INSERT INTO user (nom, genre, email, password, verification, telephone, pays, date) VALUES (\''
+        . $nom . '\', \''
+        . $genre . '\', \''
+        . $email . '\', \''
+        . $password . '\', \''
+        . $verification . '\', \''
+        . $telephone . '\', \''
+        . $pays . '\', \''
+        . 'NOW()' . '\')';
+    if(!($dbResult = mysqli_query($dbLink, $query)))
+    {
+        echo 'Erreur dans requête<br />';
+        // Affiche le type d'erreur.
+        echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+        // Affiche la requête envoyée.
+        echo 'Requête : ' . $query . '<br/>';
+        exit();
+    }
+    else {
+        echo $nom.', votre requête a bien été enregistrée.';
+    }
+}
 
 ?>
