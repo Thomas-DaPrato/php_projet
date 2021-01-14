d55d772978971f1d11a1ca52369d8f4c712b464f
Thomas-DaPrato <daprato.thomas05@gmail.com>
2021-01-14 11:38:26 +0100

    update rechercher_tag.php

--- code/recherche_tag.php
+++ code/recherche_tag.php
@@ -2,4 +2,8 @@
 $link = mysqli_connect('localhost','root','','projet_web_bd');
 $tag = $_POST['rechercher_tag'];
 $querry = 'SELECT id_msg from tag where \'' . $tag . '\'';
+while ($result = mysqli_fetch_assoc(mysqli_query($link,$querry)))
+{
+    echo $result['id_msg'];
+}
 ?>
