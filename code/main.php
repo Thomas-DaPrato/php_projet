c51fe25381e1775ebda918c46187670b99764bab
Thomas-DaPrato <daprato.thomas05@gmail.com>
2021-01-14 11:56:12 +0100

    update rechercher_tag.php

--- code/main.php
+++ code/main.php
@@ -3,5 +3,10 @@ start_page('VANESTARRE');
 ?>
 <button type="submit" name="action" value="connexion"><a href="login.php">Connexion</a></button>
 ici ya tous les messages présent sur le réseau social
+
+<form action="recherche_tag.php" method="post">
+    <input type="text" name="rechercher_tag" placeholder="tag">
+    <button type="submit" name="recherche">Ok</button>
+</form>
 <?php end_page();?>
 
--- code/recherche_tag.php
+++ code/recherche_tag.php
@@ -1,9 +1,18 @@
-<?php
-$link = mysqli_connect('localhost','root','','projet_web_bd');
+<?php require 'connection_BD.php';
+$link = connection('localhost','root','');
+mysqli_select_db($link,'projet_web_bd');
 $tag = $_POST['rechercher_tag'];
-$querry = 'SELECT id_msg from tag where \'' . $tag . '\'';
-while ($result = mysqli_fetch_assoc(mysqli_query($link,$querry)))
+$querry = 'SELECT id_msg from tag where \'texte_tag = ' . $tag . '\'';
+if (!($resultRequete = mysqli_query($link,$querry)))
 {
+    echo 'erreur de requete <br/>';
+    echo 'erreur :' .mysqli_error($link) . '<br/>';
+    echo 'requete : ' .$querry . '<br/>';
+}
+
+while ($result = mysqli_fetch_assoc($resultRequete))
+{
+    echo 'coucou';
     echo $result['id_msg'];
 }
 ?>
