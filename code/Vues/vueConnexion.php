efc0630e2ce30c7614fdbfaf345769accdcae284
Nelirks <raphael.lesbros@laposte.net>
2021-01-20 14:42:14 +0100

    Fusion de l'inscription et connexion en un controleur utilisateur

--- code/Vues/vueConnexion.php
+++ code/Vues/vueConnexion.php
@@ -2,11 +2,11 @@
 <fieldset>
     <h1 class="title_co">Se connecter</h1>
     <b><?php echo $A_vue['etat'] ?></b>
-    <form action="index.php?c=Connexion&a=Connexion" method="post">
+    <form action="index.php?c=Utilisateur&a=Connexion" method="post">
         <input type="text" name="pseudo" placeholder='Pseudo'/><br>
         <input type="password" name="mdp" placeholder='Mot de passe'/><br>
         <button type="submit" name="action" value="connexion">Connexion</button>
 
     </form><br/>
-    <a href="index.php?c=Inscription&a=Afficher">Pas encore inscrit ?</a>
+    <a href="index.php?c=Utilisateur&a=AfficherInscription">Pas encore inscrit ?</a>
 </fieldset>
--- code/Vues/vueInscription.php
+++ code/Vues/vueInscription.php
@@ -3,7 +3,7 @@
 <fieldset>
     <b><?php echo $A_vue['etat'] ?></b>
     <h1 class="title_co">S'inscrire</h1>
-    <form action="index.php?c=Inscription&a=Traitement" method="post">
+    <form action="index.php?c=Utilisateur&a=Inscription" method="post">
         <input type="text" name="pseudo" placeholder='Pseudo' required/><br>
         <input type="email" name="email" placeholder='Email' required><br>
         <input type="password" name="mdp" placeholder='Mot de passe' pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required  title="Doit contenir au moins un chiffre, une minuscule, une majuscule, et au moins 8 caractères" id="mdp_field_1" onchange="verifMdp()"/><br>
