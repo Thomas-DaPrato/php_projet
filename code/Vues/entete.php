b80019d1f95250fa88c9aea3c7224149c8808bb7
Thomas-DaPrato <daprato.thomas05@gmail.com>
2021-01-20 15:33:39 +0100

    update css

--- code/Vues/entete.php
+++ code/Vues/entete.php
@@ -4,10 +4,9 @@
 <nav class="nav-bar">
     <img src="Contenu/Images/logo_vanestarre.png" alt="logo">
      <ul class="menu">
-      <li class="menu_liste"><a href="index.php?c=Accueil&a=Afficher">Accueil</a></li>
-      <li class="menu_liste"><a href="index.php?c=APropos&a=Afficher">À propos</a></li>
-      <li class="menu_liste"><a href="index.php?c=Utilisateur">Connexion</a></li>
-      <li class="menu_liste"><a href="#">Admin</a></li>
+      <li class="menu_liste"><a class="lien_menu" href="index.php?c=Accueil&a=Afficher">Accueil</a></li>
+      <li class="menu_liste"><a class="lien_menu" href="index.php?c=Utilisateur">Connexion</a></li>
+      <li class="menu_liste"><a class="lien_menu" href="#">Admin</a></li>
 
     </ul>
     <div align="right">
@@ -23,7 +22,11 @@ else{
 </nav>
 
 
-<section></section>
+<section class="a_propos">
+    Coucou je m'appelle Vanessa MAUREL, plus connu sous le nom de VANESTARRE. Tu te trouve sur mon petit réseau social. <br/>
+    J'éspère que tu aimes me donner de l'argent car j'aime ça. <br/>
+    Des bisous trop <em>swag</em>
+</section>
 
 </header>
 
