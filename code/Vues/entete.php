dd612a58c00078a45c71b144d49082ced21c73a3
Thomas-DaPrato <daprato.thomas05@gmail.com>
2021-01-20 15:03:55 +0100

    update css

--- code/Vues/entete.php
+++ code/Vues/entete.php
@@ -1,17 +1,20 @@
 <header>
 
-<!-- Barre de navigation -->
+    <!-- Barre de navigation -->
+    <nav class="nav-bar">
+        <img src="Contenu/Images/logo_vanestarre.png" alt="logo">
+         <ul class="menu">
+          <li class="menu_liste"><a class="lien_menu" href="index.php?c=Accueil&a=Afficher">Accueil</a></li>
+          <li class="menu_liste"><a class="lien_menu" href="index.php?c=APropos&a=Afficher">À propos</a></li>
+          <li class="menu_liste"><a class="lien_menu" href="index.php?c=Connexion&a=SeConnecter">Connexion</a></li>
+          <li class="menu_liste"><a class="lien_menu" href="#">Admin</a></li>
+        </ul>
+    </nav>
 
-<nav class="nav-bar">
-    <img src="Contenu/Images/logo_vanestarre.png" alt="logo">
-     <ul class="menu">
-      <li class="menu_liste"><a href="index.php?c=Accueil&a=Afficher">Accueil</a></li>
-      <li class="menu_liste"><a href="index.php?c=APropos&a=Afficher">À propos</a></li>
-      <li class="menu_liste"><a href="index.php?c=Connexion&a=SeConnecter">Connexion</a></li>
-      <li class="menu_liste"><a href="#">Admin</a></li>
-    </ul>
-</nav>
-
-<section></section>
-
-</header>';
+    <!-- A propos -->
+    <section class="a_propos">
+        Coucou je m'appelle Vannessa MAUREL, plus connu sous le nom de VANESTARRE et voici mon petit réseau social. <br/>
+        Réagissez à mes messages et peut etre que vous me donnerez de l'argent pour votre plus grand plaisir. <br/>
+        Bisous trop <em>swag</em>
+    </section>
+</header>
--- code/Vues/vueAPropos.php
+++
@@ -1,4 +0,0 @@
-coucou je m'appelle Vannessa MAUREL, plus connu sous le nom de VANESTARRE et voici mon petit réseau social. <br/>
-Réagissez à mes messages et peut etre que vous me donnerez de l'argent pour votre plus grand plaisir. <br/>
-
-Bisous trop <em>swag</em>
