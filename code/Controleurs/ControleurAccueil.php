e0597b0212ba7499cd08c1fe219f8d05de26e492
Cindy-Brakchi <fradisia@aol.fr>
2021-01-25 15:40:01 +0100

    ajout des emojis

--- code/Controleurs/ControleurAccueil.php
+++ code/Controleurs/ControleurAccueil.php
@@ -7,7 +7,7 @@ final class ControleurAccueil
         Vue::montrer('vueAccueil', array());
     }
 
-    public function Poster(){
+    /*public function Poster(){
         // si le bouton est pressé
         $traitement = new Post;
         if (isset($_POST['poster'])){
@@ -15,10 +15,9 @@ final class ControleurAccueil
             $cible = '../Contenu/Bdd_Images/' .basename($_FILES['image']['name']);
             $image = $_FILES['image']['name']; //récupérer le nom de l'image
             $texte = $_POST['texte']; //récupérer le texte de l'image
-            Vue::montrer('vueAccueil', array($traitement->));
-
+            Vue::montrer('vueAccueil', array());
             move_uploaded_file($_FILES['image']['tmp_name'], $cible);// déplacer l'image téléchargée dans le dossier: Bdd_images
-    }
+    }*/
 
     public function AfficherPDC()
     {
