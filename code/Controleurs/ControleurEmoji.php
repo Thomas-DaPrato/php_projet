3eaa6b6ed1a69b921556855f2a1f6f5cef2b5f20
Thomas-DaPrato <daprato.thomas05@gmail.com>
2021-01-26 17:07:25 +0100

    ajout des réactions sur les messages (fonctionnel au passage)

---
+++ code/Controleurs/ControleurEmoji.php
@@ -0,0 +1,38 @@
+<?php
+require 'Modeles/modeleEmoji.php';
+class ControleurEmoji {
+
+    public function Increment()
+    {
+        $emoji = null;
+        $id_msg = null;
+
+        if (isset($_GET['incrementEmoji'])){
+            $emoji = $_GET['incrementEmoji'];
+        }
+
+        if (isset($_GET['id_msg'])){
+            $id_msg = $_GET['id_msg'];
+        }
+        $ajouter = new Emoji();
+
+        switch ($emoji) {
+            case 'love' :
+                $ajouter->addLove($id_msg);
+                break;
+            case 'cute' :
+                $ajouter->addCute($id_msg);
+                break;
+            case 'trop_style' :
+                $ajouter->addTrop_style($id_msg);
+                break;
+            case 'swag' :
+                $ajouter->addSwag($id_msg);
+                break;
+            default :
+                echo 'pas possible';
+        }
+
+        Vue::montrer('vueAccueil', array());
+    }
+}
