394879f64b4b58ff07ff0fcbf59be52931001d8f
Thomas-DaPrato <daprato.thomas05@gmail.com>
2021-01-27 16:39:21 +0100

    ajout des tags dans ajout msg

--- code/Controleurs/ControleurAccueil.php
+++ code/Controleurs/ControleurAccueil.php
@@ -16,13 +16,17 @@ final class ControleurAccueil
         $traitement = new Post;
         $texte = null;
         $image = null;
+        $tag = null;
         if (isset($_POST['poster'])) {
             // le chemin pour stocker l'image téléchargée
-            echo $_FILES['image']['name'];
             if (isset($_FILES['image']['name'])){
-                $cible = '../Contenu/Bdd_Images/' . basename($_FILES['image']['name']);
+                $cible = 'Contenu/Bdd_Images/' . basename($_FILES['image']['name']);
                 $image = $_FILES['image']['name']; //récupérer le nom de l'image
                 move_uploaded_file($_FILES['image']['tmp_name'], $cible);// déplacer l'image téléchargée dans le dossier: Bdd_images
+            }
+            if (isset($_POST['tag'])){
+                $tag = explode(" ",$_POST['tag']);
+
             }
             if (isset($_POST['texte']) and strlen($_POST['texte']) > 0 and strlen($_POST['texte']) <= 50){
                 $texte = $_POST['texte']; //récupérer le texte
@@ -31,7 +35,7 @@ final class ControleurAccueil
                 echo 'message inexistante ou trop long';
             }
 
-            Vue::montrer('vueAccueil', array($traitement->StockerPost($texte, $image)));
+            Vue::montrer('vueAccueil', array($traitement->StockerPost($texte, $image,$tag)));
         }
     }
 
--- code/Vues/vueAccueil.php
+++ code/Vues/vueAccueil.php
@@ -57,33 +57,20 @@ while ($message = $resultat->fetch(PDO::FETCH_ASSOC))
 }
 ?>
 </div>
-<section>
-	<form class="rechercheTag" action="index.php?" method="get">
-		<input type="hidden" name="c" value="Recherche">
-		<input type="hidden" name="a" value="Afficher">
-		<label><h1>Rechercher</h1></label>
-		<input type="search" name="tag" placeholder="&#946;Tag">
-		<select name="tri">
-			<option value="defaut">defaut</option>
-			<option value="recent">recent</option>
-		</select>
-		<input type="submit" value="loupe">
-	</form>
+<section class="rechercheTag">
+    <form action="index.php?" method="get">
+        <input type="hidden" name="c" value="Recherche">
+        <input type="hidden" name="a" value="Afficher">
+        <label><h1>Rechercher</h1></label>
+        <input type="search" name="tag" placeholder="&#946;Tag">
+        <select name="tri">
+            <option value="defaut">defaut</option>
+            <option value="recent">recent</option>
+        </select>
+        <input type="submit" value="loupe">
+    </form>
 </section>
 
-<!-- Formulaire de contact-->
-<h1>Contact</h1>
-<form class="cf" action="#" method="post">
-    <div class="moitié-gauche-cf">
-        <input type="text" id="input-nom" placeholder="Prénom Nom">
-        <input type="email" id="input-email" placeholder="Email">
-        <input type="text" id="input-sujet" placeholder="Sujet">
-    </div>
-    <div class="moitié-droite-cf">
-        <textarea name="message" type="text" id="input-message" placeholder="Message"></textarea>
-    </div>
-    <input type="submit" name="action" value="Envoyer" id="input-submit">
-</form>
 
 
 
