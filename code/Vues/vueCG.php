f375328628127909af20e5ab68febb15e3ecb06c
Cindy-Brakchi <fradisia@aol.fr>
2021-01-27 16:43:11 +0100

    Update vueCG.php

--- code/Vues/vueCG.php
+++ code/Vues/vueCG.php
@@ -1,3 +1,185 @@
-<h1>Conditions générales</h1>
+<div>
+    <h1><strong>CONDITIONS GÉNÉRALES D'UTILISATION DU SITE VANESTARRE</strong></h1>
 
+    <h3>ARTICLE 1. INFORMATIONS LÉGALES</h3>
+        <p>
+            En vertu de l'article 6 de la Loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé dans cet article l'identité des différents<br/>
+            intervenants dans le cadre de sa réalisation et de son suivi.<br/>
+            <br>
+            Le site VANESTARRE est édité par :<br/>
+            Mr Thomas Da-Prato, Mlle Cindy Brakchi, Mr Raphael Lesbros, Mr Erwann Lubrano , domicilié(e) à l'adresse suivante :<br/>
+            15 rue de la Paix, 33500 BORDEAUX.<br/>
+            Téléphone : 01.45.13.84.71 / Adresse e-mail : contact@editeur.fr.<br/>
+            <br>
+            Le directeur de publication du site est : Madame Vanessa Maurel.<br/>
+            <br>
+            Le site VANESTARRE est hébergé par : ALWAYSDATA ,<br/>
+            dont le siège est situé à l'adresse ci-après : 62 rue Tiquetonne – 75002 Paris. Numéro de téléphone : 01 84 16 23 40
+        </p>
+
+
+    <h3>ARTICLE 2. PRÉSENTATION DU SITE</h3>
+        <p>
+            Le site VANESTARRE a pour objet : Réseau social personnel
+        </p>
+
+    <h3>ARTICLE 3. CONTACT</h3>
+        <p>
+            Pour toute question ou demande d'information concernant le site, ou tout signalement de contenu ou d'activités illicites, l'utilisateur peut contacter l'éditeur à l'adresse<br/>
+            e-mail suivante : contact@siteinternet.fr ou adresser un courrier recommandé avec accusé de réception à :<br/>
+            Mr Thomas Da-Prato, Mlle Cindy Brakchi, Mr Raphael Lesbros, Mr Erwann Lubrano - 15 rue de la Paix, 33500 BORDEAUX
+        </p>
+
+    <h3>ARTICLE 4. ACCEPTATION DES CONDITIONS D'UTILISATION</h3>
+
+        <p>
+            L'accès et l'utilisation du site sont soumis à l'acceptation et au respect des présentes Conditions Générales d'Utilisation.<br/>
+            <br>
+            L'éditeur se réserve le droit de modifier, à tout moment et sans préavis, le site et des services ainsi que les présentes CGU, notamment pour s'adapter aux évolutions du<br/>
+            site par la mise à disposition de nouvelles fonctionnalités ou la suppression ou la modification de fonctionnalités existantes.<br/>
+            <br>
+            Il est donc conseillé à l'utilisateur de se référer avant toute navigation à la dernière version des CGU, accessible à tout moment sur le site.<br/>
+            En cas de désaccord avec les CGU, aucun usage du site ne saurait être effectué par l'utilisateur.
+        </p>
+
+    <h3>ARTICLE 5. ACCÈS ET NAVIGATION</h3>
+        <p>
+            L'accès au site et son utilisation sont réservés aux personnes âgées au minimum de (treize) ans.<br/>
+            L'éditeur sera en droit de demander une justification de l'âge de l'utilisateur, et ce par tout moyen.<br/>
+            <br>
+            L'éditeur met en œuvre les solutions techniques à sa disposition pour permettre l'accès au site 24 heures sur 24, 7 jours sur 7.<br/>
+            Il pourra néanmoins à tout moment suspendre, limiter ou interrompre l'accès au site ou à certaines pages de celui-ci afin de procéder à des mises à jours, des modifications<br/>
+            de son contenu ou tout autre action jugée nécessaire au bon fonctionnement du site.<br/>
+            <br>
+            La connexion et la navigation sur le site VANESTARRE valent acceptation sans réserve des présentes Conditions Générales d'Utilisation, quelques soient les moyens techniques<br/>
+            d'accès et les terminaux utilisés.<br/>
+            <br>
+            Les présentes CGU s'appliquent, en tant que de besoin, à toute déclinaison ou extension du site sur les réseaux sociaux et/ou communautaires existants ou à venir.
+        </p>
+
+    <h3>ARTICLE 6. GESTION DU SITE</h3>
+        <p>
+        Pour la bonne gestion du site, l'éditeur pourra à tout moment :
+            <ul>
+                <li>Suspendre, interrompre ou limiter l'accès à tout ou partie du site, réserver l'accès au site, ou à certaines parties du site, à une catégorie déterminée d'internaute</li>
+                <li>Supprimer toute information pouvant en perturber le fonctionnement ou entrant en contravention avec les lois nationales ou internationales, ou avec les règles de la Nétiquette</li>
+                <li>Suspendre le site afin de procéder à des mises à jour.</li>
+            </ul>
+        </p>
+
+    <h3>ARTICLE 7. SERVICES RÉSERVÉS AUX UTILISATEURS INSCRITS</h3>
+
+    <h4>1. INSCRIPTION</h4>
+        <p>
+            L'accès à certains services est conditionné par l'inscription de l'utilisateur.<br/>
+            <br>
+            L'inscription et l'accès aux services du site sont réservés exclusivement aux personnes physiques âgées au minimum de 13 (treize) ans. capables, ayant rempli et validé le formulaire<br/>
+            d'inscription disponible en ligne sur le site VANESTARRE , ainsi que les présentes Conditions Générales d'Utilisation.<br/>
+            <br>
+            Lors de son inscription, l'utilisateur s'engage à fournir des informations exactes, sincères et à jour sur sa personne et son état civil. L'utilisateur devra en outre procéder à une<br/>
+            vérification régulière des données le concernant afin d'en conserver l'exactitude.<br/>
+            <br>
+            L'utilisateur doit ainsi fournir impérativement une adresse e-mail valide, sur laquelle le site lui adressera une confirmation de son inscription à ses services. Une adresse de<br/>
+            messagerie électronique ne peut être utilisée plusieurs fois pour s'inscrire aux services.<br/>
+            <br>
+            Toute communication réalisée par VANESTARRE et ses partenaires est en conséquence réputée avoir été réceptionnée et lue par l'utilisateur. Ce dernier s'engage donc à consulter<br/>
+            régulièrement les messages reçus sur cette adresse e-mail et à répondre dans un délai raisonnable si cela est nécessaire.<br/>
+            <br>
+            Une seule inscription aux services du site est admise par personne physique.<br/>
+            <br>
+            L'utilisateur se voit attribuer un identifiant lui permettant d'accéder à un espace dont l'accès lui est réservé (ci-après "Espace personnel"), en complément de la saisie de<br/>
+            son mot de passe.<br/>
+            <br>
+            L'identifiant est définitif, en revanche le mot de passe est modifiable en ligne par l'utilisateur dans son Espace personnel. Le mot de passe est personnel et confidentiel,<br/>
+            l'utilisateur s'engage ainsi à ne pas le communiquer à des tiers.<br/>
+            <br>
+            VANESTARRE se réserve en tout état de cause la possibilité de refuser une demande d'inscription aux services en cas de non-respect par l'utilisateur des dispositions des<br/>
+            présentes Conditions Générales d'Utilisation.<br/>
+        </p>
+
+    <h4>2. DÉSINSCRIPTION</h4>
+        <p>
+            L'utilisateur régulièrement inscrit pourra à tout moment demander sa désinscription en se rendant sur la page dédiée dans son Espace personnel.<br/>
+            Toute désinscription du site sera effective immédiatement après que l'utilisateur ait rempli le formulaire prévu à cet effet.<br/>
+        </p>
+
+    <h4>3. SUPPRESSION DE L'ESPACE PERSONNEL À L'INITIATIVE DE L'ÉDITEUR DU SITE</h4>
+        <p>
+            Il est porté à la connaissance de l'utilisateur que l'éditeur se réserve le droit de supprimer l'espace personnel de tout Utilisateur qui contreviendrait aux présentes conditions<br/>
+            d'utilisation, et plus particulièrement dans les cas suivants :<br/>
+
+            <ul>
+                <li>Si l'utilisateur fait une utilisation illicite du site ;</li>
+                <li>Si l'utilisateur, lors de la création de son espace personnel, transmet volontairement des informations erronées au site ;</li>
+                <li>Si l'utilisateur n'a pas été actif sur son espace personnel depuis au moins un an.</li>
+            </ul>
+
+            Dans le cas où l'éditeur déciderait de supprimer l'espace personnel de l'utilisateur pour l'une de ces raisons, celle-ci ne saurait constituer un dommage pour l'utilisateur<br/>
+            dont le compte a été supprimé.<br/>
+            Cette suppression ne saurait constituer une renonciation aux poursuites judiciaires que l'éditeur pourrait entreprendre à l'égard de l'utilisateur étant contrevenu à ces règles.<br/>
+    </p>
+
+    <h3>ARTICLE 8. RESPONSABILITÉS</h3>
+        <p>
+            L'éditeur n'est responsable que du contenu qu'il a lui-même édité.<br/>
+            <br>
+            L'éditeur n'est pas responsable :<br/>
+            <ul>
+                <li>En cas de problématiques ou défaillances techniques, informatiques ou de compatibilité du site avec un matériel ou logiciel quel qu'il soit ;</li>
+                <li>Des dommages directs ou indirects, matériels ou immatériels, prévisibles ou imprévisibles résultant de l'utilisation ou des difficultés d'utilisation du site ou de ses services ;</li>
+                <li>Des caractéristiques intrinsèques de l'Internet, notamment celles relatives au manque de fiabilité et au défaut de sécurisation des informations y circulant ;</li>
+                <li>Des contenus ou activités illicites utilisant son site et ce, sans qu'il en ait pris dûment connaissance au sens de la Loi n° 58-282 à l'égard de traitement de données à caractère personnel.</li>
+            </ul>
+            Par ailleurs, le site ne saurait garantir l'exactitude, la complétude, et l'actualité des informations qui y sont diffusées.<br/>
+            <br>
+            L'utilisateur est responsable :<br/>
+            <ul>
+                <li>De la protection de son matériel et de ses données ;</li>
+                <li>De l'utilisation qu'il fait du site ou de ses services ;</li>
+                <li>S'il ne respecte ni la lettre, ni l'esprit des présentes CGU.</li>
+            </ul>
+        </p>
+
+    <h3>ARTICLE 9. LIENS HYPERTEXTES</h3>
+        <p>
+            Le site peut contenir des liens hypertextes pointant vers d'autres sites internet sur lesquels VANESTARRE n'exerce pas de contrôle. Malgré les vérifications préalables et régulières<br/>
+            réalisés par l'éditeur, celui-ci décline tout responsabilité quant aux contenus qu'il est possible de trouver sur ces sites.<br/>
+            <br>
+            L'éditeur autorise la mise en place de liens hypertextes vers toute page ou document de son site sous réserve que la mise en place de ces liens ne soit pas réalisée à des fins<br/>
+            commerciales ou publicitaires.<br/>
+            <br>
+            En outre, l'information préalable de l'éditeur du site est nécessaire avant toute mise en place de lien hypertexte.<br/>
+            <br>
+            Sont exclus de cette autorisation les sites diffusant des informations à caractère illicite, violent, polémique, pornographique, xénophobe ou pouvant porter atteinte à la sensibilité<br/>
+            du plus grand nombre.<br/>
+            <br>
+            Enfin, VANESTARRE se réserve le droit de faire supprimer à tout moment un lien hypertexte pointant vers son site, si le site l'estime non conforme à sa politique éditoriale.<br/>
+        </p>
+
+    <h3>ARTICLE 10 : CONFIDENTIALITÉ</h3>
+        <p>
+            En plus des présentes Conditions Générales, le site dispose d'une politique de confidentialité qui décrit la manière dont les données à caractère personnel sont traitées lorsque<br/>
+            l'utilisateur se rend sur le site, ainsi que la manière dont les cookies sont utilisés.<br/>
+            En naviguant sur le site, l'utilisateur déclare avoir également pris connaissance de la politique de confidentialité susmentionnée.<br/>
+        </p>
+
+    <h3>ARTICLE 11. PROPRIÉTÉ INTELLECTUELLE</h3>
+        <p>
+            La structuration du site mais aussi les textes, graphiques, images, photographies, sons, vidéos et applications informatiques qui le composent sont la propriété de l'éditeur et<br/>
+            sont protégés comme tels par les lois en vigueur au titre de la propriété intellectuelle.<br/>
+            <br>
+            Toute représentation, reproduction, adaptation ou exploitation partielle ou totale des contenus, marques déposées et services proposés par le site, par quelque procédé que ce soit,<br/>
+            sans l'autorisation préalable, expresse et écrite de l'éditeur, est strictement interdite et serait susceptible de constituer une contrefaçon au sens des articles L. 335-2 et suivants<br/>
+            du Code de la propriété intellectuelle. Et ce, à l'exception des éléments expressément désignés comme libres de droits sur le site.<br/>
+            <br>
+            L'accès au site ne vaut pas reconnaissance d'un droit et, de manière générale, ne confère aucun droit de propriété intellectuelle relatif à un élément du site, lesquels restent la <br/>
+            propriété exclusive de l'éditeur.<br/>
+            <br>
+            Il est interdit à l'utilisateur d'introduire des données sur le site qui modifieraient ou qui seraient susceptibles d'en modifier le contenu ou l'apparence.<br/>
+        </p>
+
+    <p>
+        <h5>Le site VANESTARRE vous souhaite une excellente navigation !</h5>
+    </p>
+</div>
 
