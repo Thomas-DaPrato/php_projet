2f84900312d35239872cc86c5c85803a69381497
Nelirks <raphael.lesbros@laposte.net>
2021-01-28 09:19:16 +0100

    Encore quelques commentaires

--- code/Modeles/modeleEmoji.php
+++ code/Modeles/modeleEmoji.php
@@ -9,22 +9,26 @@ final class Emoji
         $this->querry .= $_SESSION['pseudo'] .'\',';
     }
     public function addLove($id_msg)
+    //ajoute un emoji love à la base de données
     {
         $this->querry .= '\''.$id_msg.'\',\'love\')';
         $this->bdd->executerReq($this->querry);
     }
 
     public function addCute($id_msg) {
+    //Ajoute un emoji cute
         $this->querry .= '\''.$id_msg.'\',\'cute\')';
         $this->bdd->executerReq($this->querry);
     }
 
     public function addTrop_style($id_msg) {
+    //Ajoute un emoji TropStyle
         $this->querry .= '\''.$id_msg.'\',\'trop_style\')';
         $this->bdd->executerReq($this->querry);
     }
 
     public function addSwag($id_msg) {
+    //Ajoute un emoji swag
         $this->querry .= '\''.$id_msg.'\',\'swag\')';
         $this->bdd->executerReq($this->querry);
     }
