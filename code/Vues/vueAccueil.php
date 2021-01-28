b2a420835e074e8142e647d9180b16b78fb34562
Thomas-DaPrato <daprato.thomas05@gmail.com>
2021-01-28 09:30:27 +0100

    ajout indentation html

--- code/Vues/vueAccueil.php
+++ code/Vues/vueAccueil.php
@@ -1,13 +1,13 @@
 <section class="a_propos">
-    <br/>
-    Salut, c'est Vanessa Maurel plus connue sous le nom de Vanéstarre !<br/>
-    Tu te trouves sur mon réseau social. <br/>
-    <em>Enjoy !</em>
-    </p>
-</section>
+        <p>
+            Salut, c'est Vanessa Maurel plus connue sous le nom de Vanéstarre !<br/>
+            Tu te trouves sur mon réseau social. <br/>
+            <em>Enjoy !</em>
+        </p>
+    </section>
 
 
-<div class="container_msg">
+        <div class="container_msg">
 
 <?php
 if (isset($A_vue['corps']))
@@ -16,21 +16,21 @@ if (isset($A_vue['corps']))
 }
 
 ?>
-</div>
-<section class="rechercheTag">
-	<form  action="index.php?" method="get">
-		<input type="hidden" name="c" value="Recherche">
-		<input type="hidden" name="a" value="Afficher">
-		<input type="hidden" name="page" value="1">
-		<label><h1>Rechercher</h1></label>
-		<input type="search" name="tag" placeholder="&#946;Tag">
-		<select name="tri">
-			<option value="defaut">defaut</option>
-			<option value="recent">recent</option>
-		</select>
-		<input type="submit" value="loupe">
-	</form>
-</section>
+        </div>
+        <section class="rechercheTag">
+            <form  action="index.php?" method="get">
+                <input type="hidden" name="c" value="Recherche">
+                <input type="hidden" name="a" value="Afficher">
+                <input type="hidden" name="page" value="1">
+                <h1>Rechercher</h1>
+                <input type="search" name="tag" placeholder="&#946;Tag">
+                <select name="tri">
+                    <option value="defaut">defaut</option>
+                    <option value="recent">recent</option>
+                </select>
+                <input type="submit" value="loupe">
+            </form>
+        </section>
 
 
 
